859dcbf022522e40b42dbc20f3d9dd5bd5f13e90
guerbai <guerbai8@gmail.com>
2018-06-05 19:00:34 +0800

    LengthOfLongestSubstring

--- src/main/java/guerbai/App.java
+++ src/main/java/guerbai/App.java
@@ -9,5 +9,7 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        String s = "abc";
+        System.out.println(s.substring(0, 2));
     }
 }
