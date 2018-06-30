4a8e1fa09183be0dc3108a57f2930fa9c03d2795
guerbai <guerbai8@gmail.com>
2018-06-30 23:53:12 +0800

    easy remove

---
+++ src/main/java/guerbai/one_to_fifty/RemoveElement.java
@@ -0,0 +1,18 @@
+package guerbai.one_to_fifty;
+
+public class RemoveElement {
+
+    private static int removeElement(int[] nums, int val) {
+        int replaceIndex = 0;
+        int result = nums.length;
+        for (int i=0; i<nums.length; i++) {
+            if (nums[i]==val) {
+                result--;
+            } else {
+                nums[replaceIndex] = nums[i];
+                replaceIndex++;
+            }
+        }
+        return result;
+    }
+}
---
+++ src/main/java/guerbai/one_to_fifty/ReverseKGroup.java
@@ -0,0 +1,9 @@
+package guerbai.one_to_fifty;
+
+public class ReverseKGroup {
+
+    private static ListNode reverseKGroup(ListNode head, int k) {
+        return new ListNode(0);
+
+    }
+}
