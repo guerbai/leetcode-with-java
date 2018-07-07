991ec8d470c54c6bc43513965fbf9ae6b510873b
guerbai <guerbai8@gmail.com>
2018-07-07 23:29:02 +0800

    IsSameTree, IsSymmetric

---
+++ src/main/java/guerbai/hundredone_to_hundredfifty/IsSymmetric.java
@@ -0,0 +1,23 @@
+package guerbai.hundredone_to_hundredfifty;
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode(int x) { val = x; }
+}
+
+public class IsSymmetric {
+
+    private static boolean recursion(TreeNode left, TreeNode right) {
+        if (left == null && right == null) return true;
+        if (left == null && right != null) return false;
+        if (right == null && left != null) return false;
+        return left.val == right.val && recursion(left.left, right.right)
+                && recursion(right.left, left.right);
+    }
+
+    private static boolean isSymmetric(TreeNode root) {
+        return recursion(root, root);
+    }
+}
