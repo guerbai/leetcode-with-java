aa3633cbba7a1dd681a47eba9bb07ab9c160fd0b
guerbai <guerbai8@gmail.com>
2018-07-11 23:32:34 +0800

    initial arrays.aslist

--- src/main/java/guerbai/hundredone_to_hundredfifty/Yanghui1.java
+++ src/main/java/guerbai/hundredone_to_hundredfifty/Yanghui1.java
@@ -1,6 +1,7 @@
 package guerbai.hundredone_to_hundredfifty;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class Yanghui1 {
@@ -8,17 +9,13 @@ public class Yanghui1 {
     private static List<List<Integer>> generate(int numRows) {
         List<List<Integer>> result = new ArrayList<>();
         if (numRows == 0) return result;
-        List<Integer> row1 = new ArrayList<>();
-        row1.add(1);
-        result.add(row1);
+        result.add(Arrays.asList(1));
         for (int i=1; i<numRows; i++) {
             List<Integer> row = new ArrayList<>();
             for (int j=0; j<i+1; j++) {
                 if (j == 0 || j == i) {
                     row.add(1);
                 } else {
-                    System.out.println(i);
-                    System.out.println(j);
                     row.add(result.get(i-1).get(j-1) + result.get(i-1).get(j));
                 }
             }
