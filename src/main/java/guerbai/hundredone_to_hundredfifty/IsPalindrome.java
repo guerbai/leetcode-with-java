e3987ba9add27e6633213f785ae8186aac1badb0
guerbai <guerbai8@gmail.com>
2018-07-13 14:09:29 +0800

    IsPalindrome

---
+++ src/main/java/guerbai/hundredone_to_hundredfifty/IsPalindrome.java
@@ -0,0 +1,33 @@
+package guerbai.hundredone_to_hundredfifty;
+
+import java.util.regex.Pattern;
+
+public class IsPalindrome {
+
+    private static boolean isPalindrome(String s) {
+        s = s.toLowerCase();
+        int start = 0;
+        int end = s.length() - 1;
+        while (start < end) {
+            char startChar = s.charAt(start);
+            char endChar = s.charAt(end);
+            if (!Character.isLetter(startChar) && !Character.isDigit(startChar)) {
+                start++;
+            } else if (!Character.isLetter(endChar) && !Character.isDigit(endChar)) {
+                end--;
+            } else {
+                if (startChar != endChar) {
+                    return false;
+                }
+                start++;
+                end--;
+            }
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        String s = "ab2a";
+        System.out.println(isPalindrome(s));
+    }
+}
