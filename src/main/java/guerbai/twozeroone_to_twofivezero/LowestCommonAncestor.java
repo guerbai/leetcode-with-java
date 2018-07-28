a47118e11fa7e01819a7eae2e71ef0de94432e6c
guerbai <guerbai8@gmail.com>
2018-07-28 21:10:39 +0800

    LowestCommonAncestor

---
+++ src/main/java/guerbai/twozeroone_to_twofivezero/LowestCommonAncestor.java
@@ -0,0 +1,15 @@
+package guerbai.twozeroone_to_twofivezero;
+
+import guerbai.util.TreeNode;
+
+public class LowestCommonAncestor {
+
+    private static TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if (root.val > p.val && root.val > q.val) {
+            return lowestCommonAncestor(root.left, p, q);
+        } else if (root.val < p.val && root.val < q.val) {
+            return lowestCommonAncestor(root.right, p, q);
+        }
+        return root;
+    }
+}
