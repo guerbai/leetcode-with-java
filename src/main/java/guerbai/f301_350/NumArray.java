8afc2f722ba9e89f8d525b4261bc570a4a709644
guerbai <guerbai8@gmail.com>
2018-08-04 22:05:54 +0800

    reverseString

--- src/main/java/guerbai/f301_350/NumArray.java
+++ src/main/java/guerbai/f301_350/NumArray.java
@@ -3,9 +3,18 @@ package guerbai.f301_350;
 public class NumArray {
 
     int[] nums;
+    int[] sum;
 
     public NumArray(int[] nums) {
         this.nums = nums;
+        sum = new int[nums.length];
+        for (int i=0; i<nums.length; i++) {
+            if (i == 0) {
+                sum[i] = nums[i];
+            } else {
+                sum[i] = nums[i] + sum[i-1];
+            }
+        }
     }
 
     public int sumRange(int i, int j) {
@@ -14,5 +23,16 @@ public class NumArray {
             count += nums[c];
         }
         return count;
+    }
+
+    // 编程珠玑中的一个缓存方案。
+    public int sumRangeUseCache(int i, int j) {
+        return i > 0 ? sum[j] - sum[i-1]: sum[j];
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {-2, 0, 3, -5, 2, -1};
+        NumArray na = new NumArray(nums);
+        na.sumRangeUseCache(2, 5);
     }
 }
