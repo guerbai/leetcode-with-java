dc3afea49aa9b556739f55542b023d7a999cb689
guerbai <guerbai8@gmail.com>
2018-10-07 22:59:36 +0800

    use iterate fib way deal uniquepath

--- src/main/java/guerbai/f51_100/UniquePaths.java
+++ src/main/java/guerbai/f51_100/UniquePaths.java
@@ -2,8 +2,6 @@ package guerbai.f51_100;
 
 public class UniquePaths {
 
-    private static int recursion(int m, int n) {return 0;}
-
     // 排列组合题, choose n-1 from  (m+n-2)，用公式算就好;
     private static int uniquePaths(int m, int n) {
         double result = 1;
@@ -12,6 +10,22 @@ public class UniquePaths {
             result = result * (n+i-1)/i;
         }
         return (int) result;
+    }
+
+    private static int uniquePaths2(int m, int n) {
+        int[][] up = new int[m][n];
+        for (int i=0; i<m; i++) {
+            up[i][0] = 1;
+        }
+        for (int i=0; i<n; i++) {
+            up[0][i] = 1;
+        }
+        for (int i=1; i<m; i++) {
+            for (int j=1; j<n; j++) {
+                up[i][j] = up[i-1][j] + up[i][j-1];
+            }
+        }
+        return up[m-1][n-1];
     }
 
     public static void main(String[] args) {
