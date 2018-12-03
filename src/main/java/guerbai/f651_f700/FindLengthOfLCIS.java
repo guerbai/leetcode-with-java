950521c6ad8df3c250a88a5d1bdc502ed1a0293d
guerbai <guerbai8@gmail.com>
2018-12-03 22:11:16 +0800

    FindLengthOfLCIS

---
+++ src/main/java/guerbai/f651_f700/FindLengthOfLCIS.java
@@ -0,0 +1,22 @@
+package guerbai.f651_f700;
+
+public class FindLengthOfLCIS {
+
+    public int findLengthOfLCIS(int[] nums) {
+        if (nums.length == 0) return 0;
+        int maxL = 1;
+        int countL = 1;
+        int lastItem = nums[0];
+        for (int i=1; i<nums.length; i++) {
+            System.out.println(maxL);
+            if (nums[i] > lastItem) {
+                countL++;
+            } else {
+                maxL = Math.max(maxL, countL);
+                countL = 1;
+            }
+            lastItem = nums[i];
+        }
+        return Math.max(maxL, countL);  
+    }
+}
